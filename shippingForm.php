9a153105223bd1c6ea3acbb848b5f20d17e4023b
kthai3 <Kenneththai8@yahoo.com>
2022-02-16 18:00:04 -0500

    Created shipping form

---
+++ shippingForm.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Continental Café Home Page</title>
+<link rel="stylesheet" href="styles.css">
+<script src="Scripts/swfobject_modified.js"></script>
+<link rel="shortcut icon" href="images/Continental_Café_Logo_Finished.ico">
+</head>
+
+
+<body>
+
+</body>
+</html>
