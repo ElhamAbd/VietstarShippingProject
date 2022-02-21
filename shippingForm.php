dcbb099cb38376286bc9cba4ddf0bcb957546b91
kthai3 <Kenneththai8@yahoo.com>
2022-02-20 21:21:52 -0500

    Minor modifications to shipping form

--- shippingForm.php
+++ shippingForm.php
@@ -1,11 +1,11 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 <head>
 <meta charset="utf-8">
-<title>Continental Café Home Page</title>
+<title></title>
 <link rel="stylesheet" href="styles.css">
 <script src="Scripts/swfobject_modified.js"></script>
-<link rel="shortcut icon" href="images/Continental_Café_Logo_Finished.ico">
+<link rel="shortcut icon" href="">
 </head>
 
 
