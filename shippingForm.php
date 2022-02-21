b0fe48b3006030388a9903d28fa98ee0bfcfd922
kthai3 <Kenneththai8@yahoo.com>
2022-02-20 22:18:22 -0500

    minor modifications to shipping form

--- shippingForm.php
+++ shippingForm.php
@@ -3,13 +3,14 @@
 <head>
 <meta charset="utf-8">
 <title></title>
-<link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="shippingStyles.css">
 <script src="Scripts/swfobject_modified.js"></script>
 <link rel="shortcut icon" href="">
 </head>
 
 
-<body>
+    <body>
 
-</body>
+    </body>
+
 </html>
