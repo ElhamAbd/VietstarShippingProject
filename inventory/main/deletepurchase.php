a77d6245cbd3be476ac590d858ce47f18b35cff2
Mahesh Adhikari <maheshadhikari@Maheshs-MBP.eduroam.gmu.edu>
2022-03-02 17:13:12 -0500

    make commit

--- inventory/main/deletepurchase.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include('../connect.php');
-	$id=$_GET['id'];
-	$result = $db->prepare("DELETE FROM purchase WHERE purchase_id= :memid");
-	$result->bindParam(':memid', $id);
-	$result->execute();
-?>
